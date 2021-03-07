51be12bcebead047a0dab24c3268773678f70f24
Mian-785 <rehmanamjid1@gmail.com>
2021-03-07 12:04:05 +0500

    Here is the sample project

---
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    TextView tvsize;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar=findViewById(R.id.seekBar);
+        tvsize=findViewById(R.id.tvsize);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+             //   tvsize.setTextSize(progress);
+                tvsize.setAlpha(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
